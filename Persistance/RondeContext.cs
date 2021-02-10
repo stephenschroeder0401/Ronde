5212e72100dba99bbd2b7eff7331c2b1dcf78be9
Stephen <stephen.o.schroeder@gmail.com>
2021-02-10 09:17:34 -0700

    Create initial project

---
+++ Persistance/RondeContext.cs
@@ -0,0 +1,26 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Persistance
+{
+    public class RondeContext : DbContext
+    {
+        public RondeContext (DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //this was instructional by the tutorial on how to create a migration for seed data.. may want to remove
+            builder.Entity<Value>()
+                .HasData(
+                    new Value { Id = 1, Name = "Value 101"},
+                    new Value { Id = 2, Name = "Value 102" },
+                    new Value { Id = 3, Name = "Value 103" }
+                ); 
+        }
+    }
+}
